d3fc1b6441f5d069d3d580abb6404dc7b8a6d0ff
gangmii <8721son@naver.com>
2022-06-29 09:32:09 +0900

    camera

---
+++ src/main/java/com/example/webrtc/util/Utils.java
@@ -0,0 +1,20 @@
+package com.example.webrtc.util;
+
+import com.example.webrtc.dto.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Utils {
+    // Jackson JSON converter
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private Utils() {
+    }
+
+    public static Message getObject(final String message) throws Exception {
+        return objectMapper.readValue(message, Message.class);
+    }
+
+    public static String getString(final Message message) throws Exception {
+        return objectMapper.writeValueAsString(message);
+    }
+}
